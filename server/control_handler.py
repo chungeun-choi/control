4ebf973e3148548ae4c516b87e980383bd3707dc
chungeun <cucuridas@gmail.com>
2024-09-30 16:35:34 +0900

    fix - fixed logic that execute playbook

--- server/control_handler.py
+++ server/control_handler.py
@@ -1,8 +1,9 @@
-import uuid
-from fastapi import APIRouter, HTTPException
+import uuid, asyncio
+from fastapi import APIRouter, HTTPException, BackgroundTasks
 from pydantic import BaseModel
 from typing import List, Optional, Dict
 from executor.playbook import WebControlExecutor
+
 
 # 라우터 생성
 router = APIRouter()
@@ -17,7 +18,7 @@ class PlaybookRequest(BaseModel):
 executors: Dict[str, WebControlExecutor] = {}
 
 @router.post("/run/")
-def run_playbook(request: PlaybookRequest):
+def run_playbook(request: PlaybookRequest, background_tasks: BackgroundTasks):
     playbook_id = str(uuid.uuid4())
     executor = WebControlExecutor(
         playbook=request.playbook,
@@ -25,26 +26,37 @@ def run_playbook(request: PlaybookRequest):
         passwords=request.passwords
     )
     executors[playbook_id] = executor
-    result = executor.run_playbook()
-    return {"message": "Playbook executed", "result": result, "playbook_id": playbook_id}
+
+    def run_in_background():
+        executor.run_playbook()
+
+    background_tasks.add_task(run_in_background)
+
+    return {"message": "Playbook executed", "result": "success", "playbook_id": playbook_id}
 
 @router.post("/stop/{playbook_id}")
 def stop_playbook(playbook_id: str):
     if playbook_id not in executors:
         raise HTTPException(status_code=400, detail="Invalid playbook ID")
+
     executors[playbook_id].stop_playbook()
+
     return {"message": f"Playbook with ID {playbook_id} stopped"}
 
 @router.post("/pause/{playbook_id}")
 def pause_playbook(playbook_id: str):
     if playbook_id not in executors:
         raise HTTPException(status_code=400, detail="Invalid playbook ID")
+
     executors[playbook_id].pause_playbook()
+
     return {"message": f"Playbook with ID {playbook_id} paused"}
 
 @router.post("/restart/{playbook_id}")
 def restart_playbook(playbook_id: str):
     if playbook_id not in executors:
         raise HTTPException(status_code=400, detail="Invalid playbook ID")
+
     executors[playbook_id].restart_playbook()
+
     return {"message": f"Playbook with ID {playbook_id} restarted"}
